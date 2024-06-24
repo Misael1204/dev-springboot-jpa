41c3a2880d3cf1d98a75653fb8c6b200802d7df9
Misael <nilton_misael@hotmail.com>
2024-06-24 16:01:57 -0300

    Service layer, component registration

--- src/main/java/com/devmisael/devmisael/repositories/UserRepository.java
+++ src/main/java/com/devmisael/devmisael/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.devmisael.devmisael.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devmisael.devmisael.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
