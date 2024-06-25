a354500908839a204de985b1a39197215ff50da7
Misael <nilton_misael@hotmail.com>
2024-06-25 16:27:39 -0300

    Payment

--- src/main/java/com/devmisael/devmisael/config/TestConfig.java
+++ src/main/java/com/devmisael/devmisael/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devmisael.devmisael.entities.Category;
 import com.devmisael.devmisael.entities.Order;
 import com.devmisael.devmisael.entities.OrderItem;
+import com.devmisael.devmisael.entities.Payment;
 import com.devmisael.devmisael.entities.Product;
 import com.devmisael.devmisael.entities.User;
 import com.devmisael.devmisael.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
