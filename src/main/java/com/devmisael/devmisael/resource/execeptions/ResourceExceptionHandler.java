0360d3918583c7e29ab84b8d1c4cc0a4a089e414
Misael <nilton_misael@hotmail.com>
2024-06-26 12:04:25 -0300

    Delete Exception

--- src/main/java/com/devmisael/devmisael/resource/execeptions/ResourceExceptionHandler.java
+++ src/main/java/com/devmisael/devmisael/resource/execeptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devmisael.devmisael.services.exceptions.DatabaseException;
 import com.devmisael.devmisael.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,8 +16,15 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request ){
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request ){
 		String error = "Resourse not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request ){
+		String error = "Database Error";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
